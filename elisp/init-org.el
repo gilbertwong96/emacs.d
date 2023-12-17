3af7ae1dbe631ed2efa71f9568dce98a4833b4e0
Gilbert <gilbertwong96@icloud.com>
2023-12-17 18:10:33 +0800

    Enhance org mode

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -11,11 +11,16 @@
   :defer t
   :custom
   (org-todo-keywords '((sequence "TODO" "DOING" "DONE")))
+  :init
+  (leader-def
+    "oa" 'org-agenda
+    "oc" 'consult-org-agenda)
   )
 
 (use-package org-roam
   :straight t
   :ensure t
+  :demand t
   :defer t
   :custom
   (org-hide-emphasis-markers t)
@@ -24,23 +29,28 @@
   (org-roam-dailies-capture-templates
    '(("d" "default" entry "* %<%I:%M %p>: %?"
       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
-  ;; :bind
-  ;; (("C-c n l" . org-roam-buffer-toggle)
-  ;;  ("C-c n f" . org-roam-node-find)
-  ;;  ("C-c n i" . org-roam-node-insert)
-  ;;  :map org-roam-dailies-map
-  ;;  ("Y" . org-roam-dailies-capture-yesterday)
-  ;;  ("T" . org-roam-dailies-capture-tomorrow)
-  ;;  )
-  ;; :bind-keymap
-  ;; ("C-c n d" . org-roam-dailies-map)
   :config
   (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode)
   (org-roam-setup)
+  ;; Build the agenda list the first time for the session
+  (my/org-roam-refresh-agenda-list)
+
+  (add-to-list 'org-after-todo-state-change-hook
+               (lambda ()
+                 (when (equal org-state "DONE")
+                   (my/org-roam-copy-todo-to-today))))
   :init
   (leader-def
-    "nf" 'org-roam-node-find
-    "ni" 'org-roam-node-insert
+    "nf"  'org-roam-node-find
+    "nb"  'my/org-roam-capture-inbox
+    "ni"  'org-roam-node-insert
+    "nI"  'org-roam-node-insert-immediate
+    "np"  'my/org-roam-find-project
+    "nL"  'my/org-roam-find-life
+    "ns"  'my/org-roam-find-study
+    "ntp" 'my/org-roam-capture-project-task
+    "nts" 'my/org-roam-capture-project-task
     "ndY" 'org-roam-dailies-capture-yesterday
     "ndT" 'org-roam-dailies-capture-tomorrow
     "ndt" 'org-roam-dailies-capture-today
@@ -65,6 +75,191 @@
         org-roam-ui-follow t
         org-roam-ui-update-on-save t
         org-roam-ui-open-on-start t))
+
+(defun org-roam-node-insert-immediate (arg &rest args)
+  "Create a new note and insert a link in the current document without opening
+the new note’s buffer"
+  (interactive "P")
+  (let ((args (push arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
+(defun my/org-roam-filter-by-tag (tag-name)
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  (mapcar #'org-roam-node-file
+          (seq-filter
+           (my/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun my/org-roam-refresh-agenda-list ()
+  (interactive)
+  (setq org-agenda-files
+        (append (my/org-roam-list-notes-by-tag "Project")
+                (my/org-roam-list-notes-by-tag "Study")
+                (my/org-roam-list-notes-by-tag "Life")
+                )))
+
+(defun my/org-roam-project-finalize-hook ()
+  "Adds the captured project file to `org-agenda-files' if the
+capture was not aborted."
+  ;; Remove the hook since it was added temporarily
+  (remove-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Add project file to the agenda list if the capture was confirmed
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-file-name)))))
+
+(defun my/org-roam-find-project ()
+  "Find roam nodes of projects"
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "Project")
+   nil
+   :templates
+   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n#+category: Project\n#+filetags: Project")
+      :unnarrowed t)))
+  )
+
+(defun my/org-roam-find-study ()
+  "Find roam nodes of study plan"
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "Study")
+   nil
+   :templates
+   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n#+category: Study\n#+filetags: Study")
+      :unnarrowed t)))
+  )
+
+(defun my/org-roam-find-life()
+  "Find roam nodes of life plan"
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "Life")
+   nil
+   :templates
+   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n#+category: Life\n#+filetags: Life")
+      :unnarrowed t)))
+  )
+
+(defun my/org-roam-capture-inbox ()
+  (interactive)
+  (org-roam-capture-
+   :node (org-roam-node-create)
+   :templates '(("i" "inbox" plain "* %?"
+                 :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
+
+(defun my/org-roam-capture-project-task ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Capture the new task, creating the project file if necessary
+  (org-roam-capture-
+   :node (org-roam-node-read
+          nil
+          (my/org-roam-filter-by-tag "Project"))
+   :templates '(("p" "project" plain "** TODO %?"
+                 :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                        "#+title: ${title}\n#+category: Project\n#+filetags: Project"
+                                        ("Tasks"))))))
+
+(defun my/org-roam-capture-study-task ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Capture the new task, creating the project file if necessary
+  (org-roam-capture-
+   :node (org-roam-node-read
+          nil
+          (my/org-roam-filter-by-tag "Study"))
+   :templates '(("p" "study" plain "** TODO %?"
+                 :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                        "#+title: ${title}\n#+category: Study\n#+filetags: Study"
+                                        ("Tasks"))))))
+
+(defun my/org-roam-capture-life-task ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Capture the new task, creating the project file if necessary
+  (org-roam-capture-
+   :node (org-roam-node-read
+          nil
+          (my/org-roam-filter-by-tag "Life"))
+   :templates '(("p" "life" plain "** TODO %?"
+                 :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                        "#+title: ${title}\n#+category: Life\n#+filetags: Life"
+                                        ("Tasks"))))))
+
+(defun my/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+        (org-roam-dailies-capture-templates
+          '(("t" "tasks" entry "%?"
+             :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        (org-after-refile-insert-hook #'save-buffer)
+        today-file
+        pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point)))
+
+    ;; Only refile if the target file is different than the current file
+    (unless (equal (file-truename today-file)
+                   (file-truename (buffer-file-name)))
+      (org-refile nil nil (list "Tasks" today-file nil pos)))))
+
+(defun my/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+        (org-roam-dailies-capture-templates
+          '(("t" "tasks" entry "%?"
+             :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        (org-after-refile-insert-hook #'save-buffer)
+        today-file
+        pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point)))
+
+    ;; Only refile if the target file is different than the current file
+    (unless (equal (file-truename today-file)
+                   (file-truename (buffer-file-name)))
+      (org-refile nil nil (list "Tasks" today-file nil pos)))))
 
 (provide 'init-org)
 ;;; init-org.el ends here
