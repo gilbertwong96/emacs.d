e6bf14d8bfc468cafdf89f92774e3f0c5442346d
Gilbert <gilbertwong96@icloud.com>
2023-12-17 18:10:42 +0800

    Support java-mode

---
+++ elisp/init-java.el
@@ -0,0 +1,13 @@
+;;; init-java.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Gilbert's Emacs Configuration
+
+;;; Code:
+
+
+(add-hook 'java-mode 'eglot-ensure)
+
+(provide 'init-java)
+;;; init-java.el ends here
