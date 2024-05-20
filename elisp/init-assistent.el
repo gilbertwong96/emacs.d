0a236ea6fce24fdf5b0baa56d9622ed4f9467204
Gilbert <gilbertwong96@icloud.com>
2024-05-20 21:48:18 +0800

    Disable the copilot packages

--- elisp/init-assistent.el
+++ elisp/init-assistent.el
@@ -6,25 +6,25 @@
 
 ;;; Code:
 
-(use-package copilot
-  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :ensure t
-  :defer t
-  ;; :bind (:map copilot-completion-map
-  ;;             ("M-j" . 'copilot-accept-completion)
-  ;;             )
-              ;; ("TAB" . 'copilot-accept-completion)
-              ;; ("C-TAB" . 'copilot-accept-completion-by-word)
-              ;; ("C-<tab>" . 'copilot-accept-completion-by-word)))
-  :custom
-  (copilot-idle-delay 1 "Set Copilot-idle-delay to 1s")
-  :hook
-  (erlang-mode . copilot-mode)
-  :config
-  (setq copilot-max-char 1000000)
-  (general-def 'insert copilot-mode-map
-    "M-RET" 'copilot-accept-completion)
-  )
+;; (use-package copilot
+;;   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+;;   :ensure t
+;;   :defer t
+;;   ;; :bind (:map copilot-completion-map
+;;   ;;             ("M-j" . 'copilot-accept-completion)
+;;   ;;             )
+;;               ;; ("TAB" . 'copilot-accept-completion)
+;;               ;; ("C-TAB" . 'copilot-accept-completion-by-word)
+;;               ;; ("C-<tab>" . 'copilot-accept-completion-by-word)))
+;;   :custom
+;;   (copilot-idle-delay 1 "Set Copilot-idle-delay to 1s")
+;;   :hook
+;;   (erlang-mode . copilot-mode)
+;;   :config
+;;   (setq copilot-max-char 1000000)
+;;   (general-def 'insert copilot-mode-map
+;;     "M-RET" 'copilot-accept-completion)
+;;   )
 
 (use-package imenu-list
   :straight t
