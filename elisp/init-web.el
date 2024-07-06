510102204463a11b99f62b3b72cec00424f9b78d
Gilbert <gilbertwong96@icloud.com>
2024-07-06 15:53:27 +0800

    Enable tide mode when js2-minor-mode enabled

--- elisp/init-web.el
+++ elisp/init-web.el
@@ -12,7 +12,7 @@
   :straight t
   :config
   ;; Make js2-mode as major mode for JavaScript editing
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-minor-mode))
   ;; Hook it in for shell scripts running via node.js
   (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
   )
@@ -37,6 +37,7 @@
   :hook
   ((typescript-ts-mode . tide-setup)
    (tsx-ts-mode . tide-setup)
+   (js2-minor-mode . tide-setup)
    (typescript-ts-mode . tide-hl-identifier-mode)
    (before-save . tide-format-before-save)
    )
