0b108d3b7553575c2376ea6e979ad8cf770db9a5
Gilbert <gilbertwong96@icloud.com>
2024-09-27 10:14:16 +0800

    Do not enable evil surround mode in evil-mode

--- elisp/init-evil.el
+++ elisp/init-evil.el
@@ -19,7 +19,6 @@
   (after-init . evil-mode)
   :after general
   :config
-  (global-evil-surround-mode t)
   (defalias #'forward-evil-word #'forward-evil-symbol)
   ;; make evil-search-word look for symbol rather than word boundaries
   (setq-default evil-symbol-word-search t)
@@ -87,7 +86,7 @@
 
 (use-package evil-surround
   :straight t
-  :defer t
+  :after evil
   :config
   (global-evil-surround-mode t))
 
