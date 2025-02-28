56fe04556ccfbb722d39367751a9f250bf97aef6
Gilbert <gilbertwong96@icloud.com>
2025-02-28 10:36:28 +0800

    Add flutter and dart config

---
+++ elisp/init-flutter.el
@@ -0,0 +1,14 @@
+;;; Package --- init-flutter.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Enhance Elixir Developing
+
+;;; Code:
+
+
+(use-package dart-mode
+  :straight t)
+
+(provide 'init-flutter)
+;;; init-flutter.el ends here
