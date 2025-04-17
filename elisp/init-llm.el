702365b029a9c184ceb6e7a577220a9bc5db9633
Gilbert <gilbertwong96@icloud.com>
2025-04-17 14:24:57 +0800

    Update init-llm

--- elisp/init-llm.el
+++ elisp/init-llm.el
@@ -12,11 +12,17 @@
   (string-trim
    (shell-command-to-string "op read op://Personal/OpenRouter-Gemini/credential")))
 
+(defun get-gemini-api-key ()
+  "Get OpenRouter API Key from 1password."
+  (string-trim
+   (shell-command-to-string "op read 'op://Personal/Google AI Studio API/credential'")))
+
 (use-package aidermacs
   :straight t
   :defer t
   :config
   (setenv "OPENROUTER_API_KEY" (get-openrouter-api-key))
+  ;; (setenv "GEMINI_API_KEY" (get-gemini-api-key))
   :custom
   (aidermacs-use-architect-mode t)
   (aidermacs-default-model "openrouter/google/gemini-2.5-pro-exp-03-25:free")
@@ -24,7 +30,6 @@
   (leader-def
     "a"  '(:ignore t :which-key "aidermacs")
     "aa" '(aidermacs-transient-menu :which-key "Popup Aidermacs menu")))
-
 
 
 (provide 'init-llm)
